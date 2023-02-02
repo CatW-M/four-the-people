92d03a16ed9a358c041cef9256d08b812b136796
CatW-M <wheelermushlin@gmail.com>
2023-02-02 16:40:26 -0700

    Added middleware

---
+++ config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+    'http://localhost:3005'
+    //other allowed urls here
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const PORT = process.env.PORT || 3005;
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+
+
+app.use(logger)
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(corsOptions))
+
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/root'))
 
@@ -18,4 +30,5 @@ app.all('*', (req, res) => {
     }
 })
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT} `));
+app.use(errorHandler)
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
