3980778239d86aeeb60994af2524032b37e30c27
CatW-M <wheelermushlin@gmail.com>
2023-02-02 20:48:51 -0700

    Added connection to MongoDB

--- server.js
+++ server.js
@@ -2,21 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
-const { logger } = require('./middleware/logger');
+const { logger, logEvents } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/dbConn');
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 3005;
+
+connectDB();
 
 console.log(process.env.NODE_ENV);
 
+app.use(logger);
 
+app.use(express.json());
 
-app.use(logger)
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors(corsOptions))
+app.use(cookieParser());
+
+app.use(cors(corsOptions));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
@@ -34,4 +40,13 @@ app.all('*', (req, res) => {
 })
 
 app.use(errorHandler)
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+mongoose.connection.once('open', () => {
+    console.group('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
