72c06a9cc2bdd279a9f907b50a36d226a4a450a3
CatW-M <wheelermushlin@gmail.com>
2023-02-02 23:07:33 -0700

    Added API controllers and routers

--- models/User.js
+++ models/User.js
@@ -1,38 +1,28 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
+const mongoose = require('mongoose')
 
-const SALT_ROUNDS = 6;
-
-const userSchema = new Schema({
-  name: {type: String, required: true},
-  email: {
-    type: String,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-}, 
-{
-  timestamps: true,
-  toJSON: {
-    transform: function(doc, ret) {
-      delete ret.password;
-      return ret;
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        required: true
+      },
+    active: {
+        type: Boolean,
+        default: true
     }
-  }
-});
+},
+{
+    timestamps: true,
+})
 
-userSchema.pre('save', async function(next) {
-  // 'this' is the user document
-  if (!this.isModified('password')) return next();
-  // Replace the password with the computed hash
-  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
-});
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
--- server.js
+++ server.js
@@ -24,9 +24,13 @@ app.use(cookieParser());
 
 app.use(cors(corsOptions));
 
+//Routes
+
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.use('/', require('./routes/root'))
+app.use('/', require('./routes/root'));
+
+app.use('/users', require('./routes/userRoutes'));
 
 app.all('*', (req, res) => {
     res.status(404)
