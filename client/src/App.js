63461270e19c278971b81aa292a56dcfa641f9a1
CatW-M <wheelermushlin@gmail.com>
2023-02-03 10:26:33 -0700

    Frontend(Client) init commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+
+
+
+function App() {
+  return (
+        <Home />
+  );
+}
+
+export default App;
