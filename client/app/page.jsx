aacdeb2ad9a174a95dd293075ee1cceb476b8c3f
CatW-M <wheelermushlin@gmail.com>
2023-02-06 18:57:55 -0700

    Fix: Error with naming

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main>
-      <h1>Testing, Testing, Eins, Zwei, Drei</h1>
+      <h1 className="text-lg items-center place-content-center py-4 m-2">Testing, Testing, Eins, Zwei, Drei</h1>
     </main>
   )
 }
